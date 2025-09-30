ccb11e7f063519c4e5c2b765f6e6e53878e486cb
Gary Pearson <gary@lomi-lomi.co.uk>
2025-09-30 14:57:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(), // ✅ enables importing SVGs as React components
+  ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
